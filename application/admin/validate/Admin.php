00b08df440bf209ba1a91354a9495528e5d4fe8f
李帆 <48155038+LIFANFANFANFAN@users.noreply.github.com>
2019-05-17 15:56:19 +0800

    栏目功能添加
    添加了栏目功能，完善了管理员添加时不得重复的验证

--- application/admin/validate/Admin.php
+++ application/admin/validate/Admin.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 namespace app\admin\validate;
 use think\Validate;
 class Admin extends Validate
 {
-	// 验证规则
     protected $rule = [
-    	'username' => 'require|max:25',
-    	'password' => 'require'
+        'username'  =>  'require|max:25|unique:admin',
+        'password' =>  'require',
     ];
 
-    // 错误提示
-    protected $message = [
-    	'username.require' => '管理员名称必须填写',
-    	'username.max' => '管理员名称长度不得大于25位',
-    	'password.require' => '管理员密码必须填写', 
+    protected $message  =   [
+        'username.require' => '管理员名称必须填写',
+        'username.max' => '管理员名称长度不得大于25位',
+        'username.unique' => '管理员名称不得重复',
+        'password.require' => '管理员密码必须填写',
+
     ];
 
-    //验证场景设置
     protected $scene = [
-    	'add' => ['username'],
-        'edit' => ['username'],
+        'add'  =>  ['username'=>'require|unique:admin','password'],
+        'edit'  =>  ['username'=>'require|unique:admin'],
     ];
-   
-}
 
- ?>
+
+
+
+}
---
+++ runtime/temp/9b48677a017fcfd43056ad86ee831705.php
@@ -0,0 +1,259 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:90:"D:\phpstudy\PHPTutorial\WWW\Personal-Blog\public/../application/admin\view\links\edit.html";i:1557767570;s:83:"D:\phpstudy\PHPTutorial\WWW\Personal-Blog\application\admin\view\common\header.html";i:1557767570;s:84:"D:\phpstudy\PHPTutorial\WWW\Personal-Blog\application\admin\view\common\sidebar.html";i:1557767570;}*/ ?>
+<!DOCTYPE html>
+<html><head>
+        <meta charset="utf-8">
+    <title>童老师ThinkPHP交流群：484519446</title>
+
+    <meta name="description" content="Dashboard">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--Basic Styles-->
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/bootstrap.css" rel="stylesheet">
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/font-awesome.css" rel="stylesheet">
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/weather-icons.css" rel="stylesheet">
+
+    <!--Beyond styles-->
+    <link id="beyond-link" href="http://127.0.0.1/Personal-Blog/public/static/admin/style/beyond.css" rel="stylesheet" type="text/css">
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/demo.css" rel="stylesheet">
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/typicons.css" rel="stylesheet">
+    <link href="http://127.0.0.1/Personal-Blog/public/static/admin/style/animate.css" rel="stylesheet">
+    
+</head>
+<body>
+    <!-- 头部 -->
+    <div class="navbar">
+    <div class="navbar-inner">
+        <div class="navbar-container">
+            <!-- Navbar Barnd -->
+            <div class="navbar-header pull-left">
+                <a href="#" class="navbar-brand">
+                    <small>
+                            <img src="http://127.0.0.1/Personal-Blog/public/static/admin/images/logo.png" alt="">
+                        </small>
+                </a>
+            </div>
+            <!-- /Navbar Barnd -->
+            <!-- Sidebar Collapse -->
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="collapse-icon fa fa-bars"></i>
+            </div>
+            <!-- /Sidebar Collapse -->
+            <!-- Account Area and Settings -->
+            <div class="navbar-header pull-right">
+                <div class="navbar-account">
+                    <ul class="account-area">
+                        <li>
+                            <a class="login-area dropdown-toggle" data-toggle="dropdown">
+                                <div class="avatar" title="View your public profile">
+                                    <img src="http://127.0.0.1/Personal-Blog/public/static/admin/images/adam-jansen.jpg">
+                                </div>
+                                <section>
+                                    <h2><span class="profile"><span>admin</span></span></h2>
+                                </section>
+                            </a>
+                            <!--Login Area Dropdown-->
+                            <ul class="pull-right dropdown-menu dropdown-arrow dropdown-login-area">
+                                <li class="username"><a>David Stevenson</a></li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/logout.html">
+                                            退出登录
+                                        </a>
+                                </li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/changePwd.html">
+                                            修改密码
+                                        </a>
+                                </li>
+                            </ul>
+                            <!--/Login Area Dropdown-->
+                        </li>
+                        <!-- /Account Area -->
+                        <!--Note: notice that setting div must start right after account area list.
+                            no space must be between these elements-->
+                        <!-- Settings -->
+                    </ul>
+                </div>
+            </div>
+            <!-- /Account Area and Settings -->
+        </div>
+    </div>
+</div>
+    <!-- /头部 -->
+    
+    <div class="main-container container-fluid">
+        <div class="page-container">
+            <!-- Page Sidebar -->
+            <div class="page-sidebar" id="sidebar">
+    <!-- Page Sidebar Header-->
+    <div class="sidebar-header-wrapper">
+        <input class="searchinput" type="text">
+        <i class="searchicon fa fa-search"></i>
+        <div class="searchhelper">Search Reports, Charts, Emails or Notifications</div>
+    </div>
+    <!-- /Page Sidebar Header -->
+    <!-- Sidebar Menu -->
+    <ul class="nav sidebar-menu">
+        <!--Dashboard-->
+        <li>
+            <a href="#" class="menu-dropdown">
+                <i class="menu-icon fa fa-user"></i>
+                <span class="menu-text">管理员</span>
+                <i class="menu-expand"></i>
+            </a>
+            <ul class="submenu">
+                <li>
+                    <a href="<?php echo url('admin/lst'); ?>">
+                        <span class="menu-text">
+                            管理列表                                    
+                        </span>
+                        <i class="menu-expand"></i>
+                    </a>
+                </li>
+            </ul>                            
+        </li> 
+
+        <li>
+            <a href="#" class="menu-dropdown">
+                <i class="menu-icon fa fa-file-text"></i>
+                <span class="menu-text">文档</span>
+                <i class="menu-expand"></i>
+            </a>
+            <ul class="submenu">
+                <li>
+                    <a href="/admin/document/index.html">
+                        <span class="menu-text">
+                            文章列表                                    
+                        </span>
+                        <i class="menu-expand"></i>
+                    </a>
+                </li>
+            </ul>                            
+        </li> 
+
+
+        <li>
+            <a href="#" class="menu-dropdown">
+                <i class="menu-icon fa fa-link"></i>
+                <span class="menu-text">友情链接</span>
+                <i class="menu-expand"></i>
+            </a>
+            <ul class="submenu">
+                <li>
+                    <a href="<?php echo url('Links/lst'); ?>">
+                        <span class="menu-text">
+                            链接列表                                    
+                        </span>
+                        <i class="menu-expand"></i>
+                    </a>
+                </li>
+            </ul>                            
+        </li> 
+        
+        <li>
+            <a href="#" class="menu-dropdown">
+                <i class="menu-icon fa fa-gear"></i>
+                <span class="menu-text">系统</span>
+                <i class="menu-expand"></i>
+            </a>
+            <ul class="submenu">
+                <li>
+                    <a href="/admin/document/index.html">
+                        <span class="menu-text">
+                            配置                                   
+                        </span>
+                        <i class="menu-expand"></i>
+                    </a>
+                </li>
+            </ul>                            
+        </li>                        
+        
+                               
+        
+    </ul>
+    <!-- /Sidebar Menu -->
+</div>
+            <!-- /Sidebar Menu -->
+            </div>
+            <!-- /Page Sidebar -->
+            <!-- Page Content -->
+            <div class="page-content">
+                <!-- Page Breadcrumb -->
+                <div class="page-breadcrumbs">
+                    <ul class="breadcrumb">
+                                        <li>
+                        <a href="#">系统</a>
+                    </li>
+                                        <li>
+                        <a href="<?php echo url('links/lst'); ?>">链接管理</a>
+                    </li>
+                                        <li class="active">修改链接</li>
+                                        </ul>
+                </div>
+                <!-- /Page Breadcrumb -->
+
+                <!-- Page Body -->
+                <div class="page-body">
+                    
+<div class="row">
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+        <div class="widget">
+            <div class="widget-header bordered-bottom bordered-blue">
+                <span class="widget-caption">修改链接</span>
+            </div>
+            <div class="widget-body">
+                <div id="horizontal-form">
+                    <form class="form-horizontal" role="form" action="" method="post">
+                        <input type="hidden" name="id" value="<?php echo $Links['id']; ?>">
+                        <div class="form-group">
+                            <label for="username" class="col-sm-2 control-label no-padding-right">链接标题</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" id="title" placeholder="" name="title"  type="text" value="<?php echo $Links['title']; ?>">
+                            </div>
+                            <p class="help-block col-sm-4 red">* 必填</p>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="group_id" class="col-sm-2 control-label no-padding-right">链接地址</label>
+                            <div class="col-sm-6">
+                                <input class="form-control" id="url" placeholder="" name="url"  type="text" value="<?php echo $Links['url']; ?>">
+                            </div>
+                            <p class="help-block col-sm-4 red">* 必填</p>
+                        </div>  
+
+                        <div class="form-group">
+                            <label for="group_id" class="col-sm-2 control-label no-padding-right">链接描述</label>
+                            <div class="col-sm-6">
+                                <textarea class="form-control" name="desc"><?php echo $Links['desc']; ?></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">保存信息</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+                </div>
+                <!-- /Page Body -->
+            </div>
+            <!-- /Page Content -->
+        </div>  
+    </div>
+
+        <!--Basic Scripts-->
+    <script src="http://127.0.0.1/Personal-Blog/public/static/admin/style/jquery_002.js"></script>
+    <script src="http://127.0.0.1/Personal-Blog/public/static/admin/style/bootstrap.js"></script>
+    <script src="http://127.0.0.1/Personal-Blog/public/static/admin/style/jquery.js"></script>
+    <!--Beyond Scripts-->
+    <script src="http://127.0.0.1/Personal-Blog/public/static/admin/style/beyond.js"></script>
+    
+
+
+</body></html>
